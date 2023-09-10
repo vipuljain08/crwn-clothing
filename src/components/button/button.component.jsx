71b92b3c31467c98bee01d4e2a2b0193b11e8a75
Vipul Jain <vupuljain2001@gmail.com>
2023-09-10 10:55:15 +0530

    Add netlify serverless functions and stripe payment method in our website.

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,4 +1,4 @@
-import { BaseButton, GoogleSignInButton, InvertedButton } from "./button.style";
+import { BaseButton, GoogleSignInButton, InvertedButton, ButtonSpinner } from "./button.style";
 
 export const BUTTON_TYPES_CLASSES = {
   base: "base",
@@ -13,9 +13,9 @@ const getButton = (buttonType = BUTTON_TYPES_CLASSES.base) =>
     [BUTTON_TYPES_CLASSES.inverted]: InvertedButton,
   }[buttonType]);
 
-const Button = ({ children, buttonType, ...otherProps }) => {
+const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
   const CustomButton = getButton(buttonType);
-  return <CustomButton {...otherProps}>{children}</CustomButton>;
+  return <CustomButton disabled={isLoading} {...otherProps}>{isLoading ? <ButtonSpinner/> : children}</CustomButton>;
 };
 
 export default Button;
